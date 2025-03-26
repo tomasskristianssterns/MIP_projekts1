3cabe73cf19389070c007b6a8bf1207e776a2209
Germans Veidemans <germans.veidemans@edu.rtu.lv>
2025-03-26 21:11:05 +0200

    Version 0.0.2

--- app.py
+++ app.py
@@ -1,40 +1,121 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
+import random
 
-window_login = tk.Tk()
-window_login.title("Login Form")
-window_login.geometry("400x250")
-window_login.resizable(width=False, height=False)
+LEFT_BOUND = 20_000
+RIGHT_BOUND = 30_000
 
-window_login.configure(bg="#CEEDC7")
-style_login = ttk.Style()
-style_login.theme_create("loggy", parent="alt", settings={
-    "TButton": {
-        "configure": {"font": ("Calibri Light", 10, "bold"), "foreground": "black", "background": "#86C8BC"},
-        "map": {"background": [("disabled", "black"), ("pressed", "#CBAF87"), ("active", "#E7DEC8")],
-                "relief": [("pressed", "sunken"), ("!pressed", "raised")]}}})
-style_login.theme_use("loggy")
+VALID_DIVISORS = [2, 3, 4]
+MAX_STARTING_NUMBERS = 5
 
-global message, username, password
+def is_valid_number(number: int) -> bool:
+  for divisor in VALID_DIVISORS:
+    if number % divisor == 0:
+      return True
+  return False
 
-message = tk.StringVar()
-username = tk.StringVar()
-password = tk.StringVar()
+def get_number() -> int:
+  genereted_number = random.randint(LEFT_BOUND, RIGHT_BOUND)
+  while not is_valid_number(genereted_number):
+    genereted_number = random.randint(LEFT_BOUND, RIGHT_BOUND)
+  return genereted_number
 
-tk.Label(window_login, text="Labdien, lietotājs!", background="#CEEDC7", font=('Calibri Light', 17)).grid(column=0, row=1, padx= 120.0, sticky="w")
-tk.Label(window_login, text="Lūdzu pielogojies", background="#CEEDC7", font=('Calibri Light', 17)).grid(column=0, row=2, padx=120.0, sticky="w")
+def numbers_for_select():
+  starting_numbers = [get_number() for _ in range(MAX_STARTING_NUMBERS)]
+  return starting_numbers
+    
+def Start_game():
+    print(selected_number.get(), selected_algorithm.get(), selected_first.get())
+        
 
-tk.Label(window_login, text="Username", background="#CEEDC7", font=('Calibri Light', 15, 'bold')).grid(column=0, row=3, padx= 10.0, sticky="w")
-tk.Entry(window_login, textvariable=username, width=24, font=('Calibri Light', 15)).grid(column=0, row=3, padx=110.0,sticky="w")
-tk.Label(window_login, text="Password", background="#CEEDC7",font=('Calibri Light', 15, 'bold')).grid(column=0, row=4, padx=10.0, sticky="w")
-tk.Entry(window_login, textvariable=password, show="*", width=15, font=('Calibri Light', 15)).grid(column=0, row=4, padx=110.0, sticky="w")
+def Choose_options(root):
+    global window_first, selected_algorithm, selected_first, selected_number
+    window_first = root
+    window_first.title("Choose options")
+    window_first.geometry("400x300")
+    window_first.resizable(width=False, height=False)
 
-tk.Label(window_login, text="", textvariable=message,  background="#CEEDC7", font=('Calibri Light', 14)).grid(column=0, row=5, padx=100.0, sticky="w")
-# button_login = ttk.Button(window_login, text="Login", command=Login)
-button_login = ttk.Button(window_login, text="Login")
-button_login.place(height=40, width=70, x=170, y=200)
+    window_first.configure(bg="#CEEDC7")
+    style = ttk.Style()
+    style.theme_create("loggy", parent="alt", settings={
+        "TButton": {
+            "configure": {
+                "font": ("Calibri Light", 10, "bold"), 
+                "foreground": "black", 
+                "background": "#86C8BC"
+            },
+            "map": {
+                "background": [("disabled", "black"), ("pressed", "#CBAF87"), ("active", "#E7DEC8")],
+                "relief": [("pressed", "sunken"), ("!pressed", "raised")]
+            }
+        },
+        "TRadiobutton": {
+            "configure": {
+                "font": ("Calibri Light", 12),
+                "foreground": "black",
+                "background": "#CEEDC7",
+                "indicatorcolor": "#86C8BC"
+            },
+            "map": {
+                "background": [("active", "#E7DEC8")],
+                "foreground": [("active", "black")],
+                "indicatorcolor": [("selected", "#86C8BC"), ("pressed", "#CBAF87")]
+            }
+        }
+    })
+    
+    style.theme_use("loggy")
+    
+    tk.Label(window_first, text="Hello, player!", background="#CEEDC7", font=('Calibri Light', 17)).grid(column=0, row=1, padx= 130.0, sticky="w")
+    tk.Label(window_first, text="Choose who will start first:", background="#CEEDC7", font=('Calibri Light', 17)).grid(column=0, row=2, padx=70.0, sticky="w")
 
+    selected_first = tk.StringVar(value = "Human")
+     
+    human_btn = ttk.Radiobutton(text="Human", variable = selected_first, value="Human")
+    human_btn.place(x = 110, y = 80)
+    
+    computer_btn = ttk.Radiobutton(text="Computer", variable = selected_first, value = "Computer")
+    computer_btn.place (x = 200, y = 80)
+    
+    
+    tk.Label(window_first, text="Choose algorithm:", background="#CEEDC7", font=('Calibri Light', 17)).grid(column=0, row=3, padx= 100.0, pady = 40, sticky="w")
+    
+    selected_algorithm = tk.StringVar(value = "MinMax")
+    
+    MinMax_btn = ttk.Radiobutton(text="MinMax", variable = selected_algorithm, value="MinMax")
+    MinMax_btn.place(x = 110, y = 150)
+    
+    AlfaBeta_btn = ttk.Radiobutton(text="AlfaBeta",variable = selected_algorithm, value = "AlfaBeta")
+    AlfaBeta_btn.place (x = 200, y = 150)
 
-
-window_login.mainloop()
+        
+    tk.Label(window_first, text="Choose number:", background="#CEEDC7", font=('Calibri Light', 17)).grid(column=0, row=4, padx= 110.0, sticky="w")
+    numbers = numbers_for_select()
+    
+    selected_number = tk.IntVar()
+    selected_number.set(str(numbers[0]))
+    
+    btn1 = ttk.Radiobutton(text = str(numbers[0]), variable = selected_number, value = numbers[0])
+    btn1.place(x = 45, y = 220)
+    
+    btn2 = ttk.Radiobutton(text = str(numbers[1]), variable = selected_number, value = numbers[1])
+    btn2.place(x = 115, y = 220)
+    
+    btn3 = ttk.Radiobutton(text = str(numbers[2]), variable = selected_number, value = numbers[2])
+    btn3.place(x = 175, y = 220)
+    
+    btn4 = ttk.Radiobutton(text = str(numbers[3]), variable = selected_number, value = numbers[3])
+    btn4.place(x = 235, y = 220)
+    
+    btn5 = ttk.Radiobutton(text = str(numbers[4]), variable = selected_number, value = numbers[4])
+    btn5.place(x = 295, y = 220)
+    
+    button_start = ttk.Button(window_first, text = "Start", command = Start_game)
+    button_start.place(height = 40, width = 70, x = 165, y = 250)
+    
+    
+if __name__ == "__main__":
+    root = tk.Tk()
+    Choose_options(root)
+    root.mainloop()
